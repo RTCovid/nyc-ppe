b7f400e12d6b71845fe5484cff0047825f14b8b2
Russell Cohen <russell.r.cohen@gmail.com>
2020-04-08 18:02:36 -0400

    Add super basic demand modeling

---
+++ nyc_data/ppe/aggregations.py
@@ -0,0 +1,5 @@
+import datetime
+
+from nyc_data.ppe.models import Item
+
+## todo: add some basic rollups
--- nyc_data/ppe/models.py
+++ nyc_data/ppe/models.py
@@ -1,6 +1,7 @@
 import uuid
 from enum import Enum
 
+from django.contrib.postgres.fields import JSONField
 from django.db import models
 
 
@@ -19,11 +20,46 @@ def enum2choices(enum):
 
 
 # tightly control this column to keep the DB clean
-class ReporterType(str, Enum):
+class Item(str, Enum):
     faceshield = "faceshield"
     gown = "gown"
     gown_material = "gown_material"
+    n95_mask = "n95_mask"
+
+
+class Unit(str, Enum):
+    each = "each"
+    yard = "yard"
+    lb = "lb"
 
 
 class Purchase(BaseModel):
-    item = models.TextField(choices=enum2choices(ReporterType))
+    item = models.TextField(choices=enum2choices(Item))
+    quantity = models.IntegerField()
+    unit = models.TextField(choices=enum2choices(Unit), default=Unit.each)
+
+    delivery_date = models.DateField(null=True)
+
+    cost = models.IntegerField()
+
+    raw_data = JSONField()
+
+    counterparty = models.TextField()
+
+    delivery_to = models.TextField()
+
+
+class Hospital(BaseModel):
+    # TODO: need to figure out what resolution is needed. Could bring in the full geocoding hospital
+    # model from covidhospitalstatus
+    name = models.TextField()
+
+
+class Need(BaseModel):
+    item = models.TextField(choices=enum2choices(Item))
+    date = models.DateField()
+
+    quantity = models.IntegerField()
+    hospital = models.ForeignKey(Hospital, on_delete=models.CASCADE)
+
+    satisfied = models.BooleanField()
