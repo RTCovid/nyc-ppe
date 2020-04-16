6ff97e8dc420373dadb0a5016c4c1b70aba5c771
M. Jackson Wilkinson <jackson.wilkinson@pathai.com>
2020-04-16 07:53:52 -0400

    Datepicker on dashboard

--- nyc_data/ppe/static/script.js
+++ nyc_data/ppe/static/script.js
@@ -1,5 +1,6 @@
+var searchParams = new URLSearchParams(window.location.search);
+
 function addUrlParameter(name, value) {
-    var searchParams = new URLSearchParams(window.location.search);
     searchParams.set(name, value);
     window.location.search = searchParams.toString();
 }
@@ -29,4 +30,41 @@ $(function(){
         });
     }
 
+
+    // Datepicker
+    $(function() {
+        var start, end;
+
+        start = searchParams.get('start_date') ? moment(searchParams.get('start_date')) : moment();
+        end = searchParams.get('end_date') ? moment(searchParams.get('end_date')) : moment().add(29, 'days');
+    
+        function cb(start, end) {
+            var newloc;
+
+            $('#reportrange span').html(start.format('MMM D') + ' - ' + end.format('MMM D'));
+            searchParams.set('start_date', start.format('YYYY-MM-DD'));
+            searchParams.set('end_date', end.format('YYYY-MM-DD'));
+
+            newloc = searchParams.toString();
+            if (newloc != window.location.search.replace('?','')) {
+                window.location.search = newloc;
+            }
+        }
+    
+        $('#reportrange').daterangepicker({
+            startDate: start,
+            endDate: end,
+            ranges: {
+                'This Week': [moment().startOf('week'), moment().endOf('week')],
+                'Next Week': [moment().add(1, 'week').startOf('week'), moment().add(1, 'week').endOf('week')],
+                'Next 7 Days': [moment(), moment().add(6, 'days')],
+                'Next 30 Days': [moment(), moment().add(29, 'days')],
+                'This Month': [moment().startOf('month'), moment().endOf('month')],
+                'Next Month': [moment().add(1, 'month').startOf('month'), moment().add(1, 'month').endOf('month')]
+            }
+        }, cb);
+    
+        cb(start, end);
+    
+    });
 });
