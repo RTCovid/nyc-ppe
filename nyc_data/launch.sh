5da52b2bdd57690b5f9d7d2c880163fc018ed9e6
M. Jackson Wilkinson <jackson.wilkinson@pathai.com>
2020-04-17 15:48:35 -0400

    Testing configuration in prod!

--- nyc_data/launch.sh
+++ nyc_data/launch.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
 
-if [ "$MIGRATE" ]; then
-  python manage.py migrate
-fi
-gunicorn -b 0.0.0.0:8000 nyc_data.wsgi
+python manage.py migrate
+python manage.py collectstatic
+pipenv run gunicorn -b 0.0.0.0:8000 nyc_data.wsgi
