6ed5b348d466f7e7289458e061ab98f90524488e
Russell Cohen <russell.r.cohen@gmail.com>
2020-04-16 23:43:53 -0400

    Fix date format hint to match reality

--- nyc_data/ppe/forms.py
+++ nyc_data/ppe/forms.py
@@ -4,4 +4,4 @@ from django import forms
 class UploadFileForm(forms.Form):
     file = forms.FileField()
     name = forms.CharField(label="Your name")
-    data_current = forms.DateField(label="Data current as of MM-DD-YYYY")
+    data_current = forms.DateField(label="Data current as of YYYY-MM-DD")
